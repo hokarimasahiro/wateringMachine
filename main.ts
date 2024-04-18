b2eccb0ac8a48fe1c484c6406d4ed03790234da3
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2024-04-18 12:57:25 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,7 @@
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    非表示時間 = input.runningTime() + 2000
     時刻表示()
     かん水()
-    datalogger.deleteLog(datalogger.DeleteType.Full)
-    dataLog = true
-    basic.clearScreen()
-})
-datalogger.onLogFull(function () {
-    dataLog = false
 })
 input.onButtonPressed(Button.A, function () {
     かん水間隔 += 1
@@ -14,11 +9,29 @@ input.onButtonPressed(Button.A, function () {
         かん水間隔 = 0
     }
     watchfont.showNumber2(かん水間隔)
+    非表示時間 = input.runningTime() + 1000
 })
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     受信文字 = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     コマンド処理()
 })
+function 時刻表示２ () {
+    ds3231.getClock()
+    watchfont.showSorobanNumber(ds3231.getClockData(clockData.hour), 0, 2)
+    watchfont.showSorobanNumber(ds3231.getClockData(clockData.minute), 3, 2)
+    if (ds3231.getClockData(clockData.second) % 2 == 0) {
+        for (let カウンター = 0; カウンター <= 4; カウンター++) {
+            led.unplot(2, カウンター)
+        }
+    } else {
+        if (ds3231.getClockData(clockData.second) < 10) {
+            led.plot(2, 1)
+            led.plot(2, 3)
+        } else {
+            led.plot(2, 5 - Math.trunc(ds3231.getClockData(clockData.second) / 10))
+        }
+    }
+}
 function コマンド処理 () {
     コマンド = 受信文字.split(",")
     if (コマンド[0] == "s") {
@@ -46,10 +59,11 @@ input.onButtonPressed(Button.B, function () {
         かん水時間 = 1
     }
     basic.showNumber(かん水時間)
+    非表示時間 = input.runningTime() + 1000
 })
 function アラーム設定 () {
     ds3231.getClock()
-    if (かん水間隔 == 2) {
+    if (かん水間隔 == 0) {
         if (ds3231.getClockData(clockData.hour) >= 7 && ds3231.getClockData(clockData.hour) < 19) {
             ds3231.setAlarm(2, 19, 0)
         } else {
@@ -68,32 +82,26 @@ function 時刻表示 () {
 }
 let コマンド: string[] = []
 let 受信文字 = ""
+let 非表示時間 = 0
 let かん水間隔 = 0
 let かん水時間 = 0
-let dataLog = false
 ds3231.initDevice()
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P16, 0)
 アラーム設定()
 serial.redirectToUSB()
 radio.setGroup(80)
 led.setBrightness(32)
-dataLog = true
 かん水時間 = 3
+let コメント = "かん水時間：秒"
 かん水間隔 = 0
+コメント = "かん水間隔 0:1日2回 1～15:かん水間隔日数"
 let かん水待ち日数 = かん水間隔
 basic.showIcon(IconNames.SmallHeart)
-loops.everyInterval(60000, function () {
-    if (dataLog) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(10)
-        ds3231.getClock()
-        datalogger.log(
-        datalogger.createCV("t", ds3231.getClockData(clockData.unix)),
-        datalogger.createCV("r", pins.analogReadPin(AnalogPin.P1))
-        )
-        pins.digitalWritePin(DigitalPin.P0, 0)
-    }
-})
 basic.forever(function () {
+    if (非表示時間 <= input.runningTime()) {
+        時刻表示２()
+    }
     if (ds3231.checkAlarm(2)) {
         かん水待ち日数 += -1
         if (かん水待ち日数 <= 0) {
@@ -103,5 +111,5 @@ basic.forever(function () {
             アラーム設定()
         }
     }
-    basic.pause(60000)
+    basic.pause(1000)
 })
